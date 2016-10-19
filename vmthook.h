c8ce47ffab3df8459f908b5d795c5078200bb7a9
aixxe <me@aixxe.net>
2016-10-19 11:20:32 +0100

    Re-format code.

--- vmthook.h
+++ vmthook.h
@@ -1,62 +1,65 @@
 #ifndef VMTHOOK_H_
 #define VMTHOOK_H_
 
+#include <cstdint>
+#include <cstddef>
+#include <cstring>
 #include <memory>
 
 class VMTHook {
-private:
-    uintptr_t**                   baseclass       = nullptr;
-    std::unique_ptr< uintptr_t[]> current_vft     = nullptr;
-    uintptr_t*                    original_vft    = nullptr;
-    size_t                        total_functions = 0;
+	private:
+		std::uintptr_t** baseclass = nullptr;
+		std::unique_ptr<uintptr_t[]> current_vft = nullptr;
+		std::uintptr_t* original_vft = nullptr;
+		std::size_t total_functions = 0;
+	public:
+		VMTHook(void) = default;
 
-public:
-    VMTHook( void ) = default;
-    VMTHook( void* baseclass ) {
-        this->baseclass                           = reinterpret_cast<uintptr_t**>( baseclass );
+		VMTHook(void* baseclass) {
+			this->baseclass = reinterpret_cast<std::uintptr_t**>(baseclass);
 
-        while( reinterpret_cast<uintptr_t*>( *this->baseclass )[ this->total_functions ] )
-            ++this->total_functions;
+			while (reinterpret_cast<std::uintptr_t*>(*this->baseclass)[this->total_functions])
+				++this->total_functions;
 
-        const auto table_size = this->total_functions * sizeof( uintptr_t );
+			const std::size_t table_size = this->total_functions * sizeof(std::uintptr_t);
 
-        this->original_vft = *this->baseclass;
-        this->current_vft  = std::make_unique< uintptr_t[] >( table_size );
+			this->original_vft = *this->baseclass;
+			this->current_vft = std::make_unique<std::uintptr_t[]>(table_size);
 
-        std::memcpy( this->current_vft.get(), this->original_vft, table_size );
+			std::memcpy(this->current_vft.get(), this->original_vft, table_size);
 
-        *this->baseclass = this->current_vft.get();
-    };
+			*this->baseclass = this->current_vft.get();
+		};
 
-    ~VMTHook() {
-        *this->baseclass = this->original_vft;
-    };
+		~VMTHook() {
+			*this->baseclass = this->original_vft;
+		};
 
-    template <typename Function> inline Function GetOriginalFunction( size_t function_index ) {
-        return reinterpret_cast<Function>( this->original_vft[ function_index ] );
-    }
+		template <typename Function> inline Function GetOriginalFunction(std::size_t function_index) {
+			return reinterpret_cast<Function>(this->original_vft[function_index]);
+		}
 
-    inline bool HookFunction( void* new_function, const size_t function_index ) {
-        if( function_index > this->total_functions )
-            return false;
+		inline bool HookFunction(void* new_function, const std::size_t function_index) {
+			if (function_index > this->total_functions)
+				return false;
 
-        this->current_vft[ function_index ] = reinterpret_cast<uintptr_t>( new_function );
+			this->current_vft[function_index] = reinterpret_cast<std::uintptr_t>(new_function);
 
-        return true;
-    }
+			return true;
+		}
 
-    inline bool UnhookFunction( const size_t function_index ) {
-        if( function_index > this->total_functions )
-            return false;
+		inline bool UnhookFunction(const std::size_t function_index) {
+			if (function_index > this->total_functions)
+				return false;
 
-        this->current_vft[ function_index ] = this->original_vft[ function_index ];
+			this->current_vft[function_index] = this->original_vft[function_index];
 
-        return true;
-    }
+			return true;
+		}
 
-    inline size_t GetTotalFunctions() {
-        return this->total_functions;
-    }
+		inline std::size_t GetTotalFunctions() {
+			return this->total_functions;
+		}
 };
 
 #endif
